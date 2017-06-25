fac2e693f5250550d9ad4256cd4347ce83b94c53
guanbo.huanggb <guanbo.huanggb@alibaba-inc.com>
2017-06-25 21:59:36 +0800

    test

--- cloud.js
+++ cloud.js
@@ -27,7 +27,7 @@ AV.Cloud.define('report', function(request, response){
 		reportQuery.equalTo('userId', userId);
 		reportQuery.equalTo('reportedUserId', reportedUserId);
 		reportQuery.find().then(function(results){
-			if(results.length == 0){
+			if(!results || results.length == 0){
 				//需要扣减分数
 				var userDynamicQuery = AV.Query(UserDynamicData);
 				var user = new User();
